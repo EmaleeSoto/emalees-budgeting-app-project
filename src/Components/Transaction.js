01b62fc7b5534f7ce562f7b430403a46427158b4
EmaleeSoto <emaleesoto@gmail.com>
2022-07-27 11:48:14 -0400

    Small changes

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,24 +1,6 @@
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import React from "react";
-import axios from "axios";
-const API = process.env.REACT_APP_API_URL;
 
 export default function Transaction({ transaction, index, totalFormatter }) {
-  const navigate = useNavigate();
-
-  const handleDelete = (event) => {
-    axios
-      .delete(`${API}/transactions/${index}`)
-      .then(() => {
-        // navigate("/transactions");
-        window.location.reload();
-      })
-      .catch((err) => {
-        console.warn(err);
-      });
-  };
-
   return (
     <tr>
       <td className="table-info">{transaction.date}</td>
@@ -29,14 +11,6 @@ export default function Transaction({ transaction, index, totalFormatter }) {
         <p id={Math.sign(transaction.amount) >= 0 ? "positive" : "negative"}>
           {totalFormatter(transaction.amount)}{" "}
         </p>
-      </td>
-      <td></td>
-      <td>
-        <form>
-          {/* <button className="delete-button" onClick={handleDelete}>
-            Delete Transaction
-          </button> */}
-        </form>
       </td>
     </tr>
   );
